1699bde0f2d028e1d14d38926f66966598eb8a3e
aviaryan77 <aviaryan77@gmail.com>
2021-03-20 15:29:40 +0530

    users profile bug

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.options("*", cors());
 
 //middlewere
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(morgan("tiny"));
 app.use(authJwt());
 app.use(errorHandler);
--- helpers/jwt.js
+++ helpers/jwt.js
@@ -11,6 +11,8 @@ function authJwt() {
     path: [
       { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
       { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/users(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/orders(.*)/, methods: ["GET", "OPTIONS"] },
       `${api}/users/login`,
       `${api}/users/register`,
     ],
